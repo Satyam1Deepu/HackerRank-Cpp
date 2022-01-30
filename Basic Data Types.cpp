fd9bf706080b929bbb26a3aa522d28a2c42ed938
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-01-30 22:07:18 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,13 @@
+int main() {
+    // Complete the code.
+    int  a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+cout<<a<<endl<<b<<endl<<c<<endl;
+printf("%0.3f\n",d);
+printf("%0.9lf",e);
+    return 0;
+}
