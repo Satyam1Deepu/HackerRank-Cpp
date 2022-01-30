0b8ad5c50b5423dcf589820c9cda20f4ffbe5a54
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-01-30 22:09:38 +0530

    Create Arrays Introduction.cpp

---
+++ Arrays Introduction.cpp
@@ -0,0 +1,14 @@
+int main() {
+ int n  ;
+ cin>>n;
+ int * arr = new int [n] ;
+ for(int i = 0 ; i < n ; i++){
+     cin>>arr[i];
+ }
+ // Printing :
+   for(int i = n-1 ;i >=0 ; i--){
+       cout<<arr[i]<<" ";
+   }
+ delete []arr;
+    return 0;
+}
